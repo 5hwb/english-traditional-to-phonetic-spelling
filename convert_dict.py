6b517aa98674e2987cce8722e1ff0469368116d5
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-12-29 13:51:45 +1100

    Made more tweaks to convert_dict.py

--- convert_dict.py
+++ convert_dict.py
@@ -7,19 +7,19 @@ cmudict_to_ebeo = {
     "AA": "o", "AA0": "e", "AA1": "o", "AA2": "o",
     "AE": "á", "AE0": "e", "AE1": "á", "AE2": "á",
     "AH": "a", "AH0": "e", "AH1": "a", "AH2": "a",
-    "AO": "ó", "AO0": "e", "AO1": "ó", "AO2": "ó",
-    "AW": "à", "AW0": "e", "AW1": "à", "AW2": "à",
-    "AY": "ā", "AY0": "e", "AY1": "ā", "AY2": "ā",
+    "AO": "ó", "AO0": "ó", "AO1": "ó", "AO2": "ó",
+    "AW": "à", "AW0": "à", "AW1": "à", "AW2": "à",
+    "AY": "ā", "AY0": "ā", "AY1": "ā", "AY2": "ā",
     "B": "b",
     "CH": "c",
     "D": "d",
     "DH": "đ",
-    "EH": "é", "EH0": "e", "EH1": "é", "EH2": "é",
+    "EH": "é", "EH0": "é", "EH1": "é", "EH2": "é",
     "ER": "er", "ER0": "er", "ER1": "er", "ER2": "er",
-    "EY": "ē", "EY0": "e", "EY1": "ē", "EY2": "ē",
+    "EY": "ē", "EY0": "ē", "EY1": "ē", "EY2": "ē",
     "F": "f",
     "G": "g",
-    "HH": "h", "HH0": "e", "HH1": "h", "HH2": "h",
+    "HH": "h",
     "IH": "i", "IH0": "i", "IH1": "i", "IH2": "i",
     "IY": "í", "IY0": "i", "IY1": "í", "IY2": "í",
     "JH": "j",
@@ -28,16 +28,16 @@ cmudict_to_ebeo = {
     "M": "m",
     "N": "n",
     "NG": "ŋ",
-    "OW": "ò", "OW0": "e", "OW1": "ò", "OW2": "ò",
-    "OY": "ō", "OY0": "e", "OY1": "ō", "OY2": "ō",
+    "OW": "ò", "OW0": "ò", "OW1": "ò", "OW2": "ò",
+    "OY": "ō", "OY0": "ō", "OY1": "ō", "OY2": "ō",
     "P": "p",
     "R": "r",
     "S": "s",
     "SH": "ś",
     "T": "t",
     "TH": "ŧ",
-    "UH": "u", "UH0": "e", "UH1": "u", "UH2": "u",
-    "UW": "ú", "UW0": "e", "UW1": "ú", "UW2": "ú",
+    "UH": "u", "UH0": "u", "UH1": "u", "UH2": "u",
+    "UW": "ú", "UW0": "ú", "UW1": "ú", "UW2": "ú",
     "V": "v",
     "W": "w",
     "Y": "y",
@@ -53,11 +53,11 @@ def cmudict_entry_to_word(entry):
     new_word = ""
     for phoneme in entry.split(" "):
         new_word += cmudict_to_ebeo[phoneme]
-    
+
     # Fix certain sequences to correct pronunciation
     new_word = new_word.replace("or", "ar")
     new_word = new_word.replace("ór", "or")
-    
+
     return new_word
 
 # Open the file with the dictionary
@@ -69,7 +69,7 @@ for line in cmudict.split("\n"):
     # Skip comments
     if (line[0:3] == ";;;"):
         continue
-        
+
     # Process the line
     pair = line.split("  ")
     # Is it a valid word-pronunciation pair?
@@ -78,4 +78,3 @@ for line in cmudict.split("\n"):
                 cmudict_entry_to_word(pair[1])))
     else:
         print("{} = nothing".format(pair[0]))
-
